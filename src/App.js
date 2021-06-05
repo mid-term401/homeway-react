c5dd869b35c03e27319d05460856d768d36c29b6
haneen <haneen.khasawneh96@gmail.com>
2021-06-05 17:20:19 +0300

    Sign Up form style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import SignUpForm from './component/signUp/signUp-Haneen'
+import Profile from './component/hostProfile/host-haneen';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <SignUpForm/>
+      
+       {/* <Profile/> */}
+      
       </header>
+
+     
     </div>
+
+
   );
 }
 
